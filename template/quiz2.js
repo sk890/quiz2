6f0a67053ca4f12dadba86647985fe94157e97e5
sk890 <shk76@pitt.edu>
2016-03-28 23:51:44 -0400

    edited CSS

--- template/quiz2.js
+++ template/quiz2.js
@@ -36,26 +36,30 @@
         setTimeout(function(){
             $timeout.fadeIn('slow');
         }, 1000);
-
+        
+        
+        search_cookie()
     });
-	
-    //part 2
+    
+    //get random integer 
     function get_rand_int(min, max) {
         rand_int=Math.floor(Math.random()*(max-min+1))+min;
         return rand_int
     }
-	
-	var cookie_list = document.cookie.split(';');
-	for(var i=0; i<cookie_list.length; i++) {
-		cookie_part=cookie_list[i].split('=');
-		console.log(cookie_part[0]);
-		if (cookie_part[0].trim()=="saved_title"){
-			$('.titles').html('<h3 class="current_title">'+cookie_part[1].trim()+'</h3>');
-		}
-	}
-
+    
+    //Check Set Cookies
+    function search_cookie(){
+        var cookie_list = document.cookie.split(';');
+        for(var i=0; i<cookie_list.length; i++) {
+            cookie_part=cookie_list[i].split('=');
+            if (cookie_part[0].trim()=="saved_title"){
+                $('.titles').html('<h3 class="current_title">'+cookie_part[1].trim()+'</h3>');
+            }
+        }
+    }
+    
     $( "#main_button" ).click(function() {
-		jQuery.support.cors = true;
+        jQuery.support.cors = true; //ie support
         $.ajax({
             type: 'get',
             url: 'http://www.mattbowytz.com/simple_api.json',
@@ -68,7 +72,8 @@
             }
         });
     });
-
+    
+    //After Ajax call
     function process_titles(titles) {
         title_length=titles.length;
         rand_int=get_rand_int(0,title_length-1);
@@ -82,9 +87,7 @@
 
         $( "#keep_it" ).click(function() {
             cur_title= $(".current_title" ).text();
-			alert(cur_title);
             document.cookie = "saved_title="+cur_title+"; expires=Fri, 31 Dec 9999 23:59:59 GMT";
-            console.log(document.cookie);
         });
 
     }
